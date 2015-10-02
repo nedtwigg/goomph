a0a442c6a4150556a7c7b68f58667f1f9e031b6d
Ned Twigg <ned.twigg@diffplug.com>
2015-10-02 13:07:54 -0700

    Test build for Travis folks.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Do the Gradle build
-./gradlew build || exit 1
+echo $SOMEVAR
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/goomph" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-	# Publish the artifacts
-	./gradlew publish || exit 1
-	# Push the javadoc
-	./.ci/push-javadoc.sh
-fi
+# Do the Gradle build
+#./gradlew build || exit 1
+#
+#if [ "$TRAVIS_REPO_SLUG" == "diffplug/goomph" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+#	# Publish the artifacts
+#	./gradlew publish || exit 1
+#	# Push the javadoc
+#	./.ci/push-javadoc.sh
+#fi
